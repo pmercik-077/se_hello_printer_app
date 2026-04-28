36dfed68cfe7d1b6385951c37356cc5a1badaece
pmercik-077 <paweu7077@gmail.com>
2026-04-28 10:13:29 +0200

    views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,8 +1,17 @@
 from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
-from flask import request
+from flask import request, jsonify, render_template
 
+@app.route("/")
+def index():
+    data = {"imie": "Pawel", "msg": "Hello world!"}
+
+    if request.args.get("output") == "json":
+        return jsonify(data)
+
+    return render_template("index.html", data=data)
+    
 moje_imie = "Pawel"
 msg = "Hello World!"
 
