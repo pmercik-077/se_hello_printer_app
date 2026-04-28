cf2484a1ca935be76a75526847af831b6b898df7
pmercik-077 <paweu7077@gmail.com>
2026-04-28 10:26:49 +0200

    views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -17,6 +17,15 @@ moje_imie = "Pawel"
 msg = "Hello World!"
 
 
+@app.route('/')
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, moje_imie,
+                         output.lower())
+
+
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
